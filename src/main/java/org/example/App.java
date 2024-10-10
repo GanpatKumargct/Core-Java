1604cf4814a4ee6abcbf31c62ec83e9136649054
Ganpat Kumar <ganpatstudy@gmail.com>
2024-10-10 10:18:28 +0530

    JSON

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws JsonProcessingException {
+
+        Student student = new Student();
+        student.setName("Narayan");
+        student.setCity("Dharavi");
+        student.setCourse("JAVA - Core");
+
+        System.out.println(student);
+
+        System.out.println("------JSON Object Mapper------");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Object obj = objectMapper.writeValueAsString(student);
+        System.out.println(obj);
+
+
+
+    }
+}
